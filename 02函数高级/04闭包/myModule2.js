616fca2014c5c19d54a00ac12092b1c044bb9cfb
sunbowen <857153966@qq.com>
2020-06-18 15:14:34 +0800

    闭包和对象创建模式

---
+++ 02函数高级/04闭包/myModule2.js
@@ -0,0 +1,17 @@
+(function myModule(window){
+    // 私有的数据
+    var msg = 'Soul Mate'
+    // 操作数据的函数
+    function doSomething(){
+        console.log('dosomething'+msg.toUpperCase());
+    }
+    function doOthering () {
+        console.log('doOthering'+msg.toLowerCase())
+    }
+
+    // 向外暴露对象（给外部使用的方法）
+    window.myModule2 = {
+        doSomething:doSomething,
+        doOtherthing:doOthering
+    }
+})(window)
